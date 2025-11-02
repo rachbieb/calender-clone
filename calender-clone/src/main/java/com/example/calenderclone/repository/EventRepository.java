376824301eaf7e9de86b37d088f3b937445927cd
Panyam  karthikeya <142717867+rachbieb@users.noreply.github.com>
2025-11-02 16:12:51 +0530

    Delete calender-clone directory

--- calender-clone/src/main/java/com/example/calenderclone/repository/EventRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.calenderclone.repository;
-
-import com.example.calenderclone.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {}
